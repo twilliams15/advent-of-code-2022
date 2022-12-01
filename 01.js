8bbe9f3a000c83972b8661dabc101e06f9ba3f51
Todd Williams <twilliams@gofundme.com>
2022-12-01 09:01:05 -0800

    cleanup

--- 01.js
+++ 01.js
@@ -6,18 +6,16 @@ fs.readFile("./01-input.txt", "utf8", (err, data) => {
     return;
   }
 
-  const calorieList = data.split("\n");
-
   // --- helpers ---
 
   const toNumber = (s) => +s;
   const toSum = (a, c) => (a += c);
   const desc = (a, b) => b - a;
-  const splitOnEmpty = (arr) => {
+  const splitArray = (splitOn) => (arr) => {
     const result = [];
     let buffer = [];
     arr.forEach((val) => {
-      if (val) {
+      if (val !== splitOn) {
         buffer.push(val);
       } else {
         result.push(buffer);
@@ -26,10 +24,11 @@ fs.readFile("./01-input.txt", "utf8", (err, data) => {
     });
     return result;
   };
+  const splitOnEmpty = splitArray("");
 
   // ---
 
-  const calorieListByElf = splitOnEmpty(calorieList);
+  const calorieListByElf = splitOnEmpty(data.split("\n"));
   const result = calorieListByElf.map((cals) =>
     cals.map(toNumber).reduce(toSum)
   );
